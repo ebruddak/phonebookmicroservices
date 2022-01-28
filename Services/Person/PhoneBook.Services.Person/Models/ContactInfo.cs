067b395cc7ec0ddb02e01b714fa84785b86ce689
ebruddak <64781645+ebruddak@users.noreply.github.com>
2022-01-28 21:18:13 +0300

    Added gitİgnore and models

---
+++ Services/Person/PhoneBook.Services.Person/Models/ContactInfo.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PhoneBook.Services.Person.Models
+{
+    public class ContactInfo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ID { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PersonID { get; set; }
+
+        [BsonIgnore]
+        public Person Person { get; set; }
+        public InfoType Type { get; set; }
+        public string Content { get; set; }
+
+    }
+}
---
+++ Services/Person/PhoneBook.Services.Person/Models/InfoType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Services.Person.Models
+{
+    public class InfoType
+    {
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
