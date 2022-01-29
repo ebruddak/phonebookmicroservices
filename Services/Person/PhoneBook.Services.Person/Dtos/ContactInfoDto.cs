19b15658ba97b3d684755e327090bd110df9f0ec
ebruddak <64781645+ebruddak@users.noreply.github.com>
2022-01-29 05:51:29 +0300

    Added dtos

---
+++ Services/Person/PhoneBook.Services.Person/Dtos/ContactInfoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Services.Person.Dtos
+{
+    public class ContactInfoDto
+    {
+      
+        public string PersonID { get; set; }
+
+        public InfoTypeDto Type { get; set; }
+        public string Content { get; set; }
+    }
+}
