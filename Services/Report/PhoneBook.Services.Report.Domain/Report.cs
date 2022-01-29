aecd3de3ab28dd5546d968660aa8a24257f7e164
ebruddak <64781645+ebruddak@users.noreply.github.com>
2022-01-29 18:19:52 +0300

    Added report aggregateroot

---
+++ Services/Report/PhoneBook.Services.Report.Domain/Report.cs
@@ -0,0 +1,46 @@
+﻿using PhoneBook.Services.Report.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Services.Report.Domain
+{
+    public class Report:Entity,IAggregateRoot
+    {
+        public string UUID { get; set; }
+        public DateTime RequestTime { get; private set; }
+        public DateTime CreatedTime { get; private set; }
+
+        public bool Status { get; private set; }
+        public string ReportUrl { get; private set; }
+
+        private readonly List<ReportItem> _reportItems;
+        public IReadOnlyCollection<ReportItem> ReportItems => _reportItems;
+
+        public Report()
+        {
+        }
+        public Report( bool status,string reportUrl, DateTime createdTime)
+        {
+            _reportItems = new List<ReportItem>();
+            RequestTime = DateTime.Now;
+            Status = status;
+            ReportUrl = reportUrl;
+            CreatedTime = createdTime;
+        }
+        public void AddReportItem(string location, int personCount, int phoneNumberCount)
+        {
+       
+                var newReportItem = new ReportItem(location, personCount, phoneNumberCount);
+            _reportItems.Add(newReportItem);
+
+        }
+        public void UpdateReportStatus(bool status,string reportUrl)
+        {
+            Status = status;
+            ReportUrl = reportUrl;
+        }
+    }
+}
---
+++ Services/Report/PhoneBook.Services.Report.Domain/ReportItem.cs
@@ -0,0 +1,23 @@
+﻿using PhoneBook.Services.Report.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Services.Report.Domain
+{
+    public class ReportItem:Entity
+    {
+        public string Location { get; private set; }
+        public int PersonCount { get; private set; }
+        public int PhoneNumberCount { get; private set; }
+
+        public ReportItem(string location, int personCount, int phoneNumberCount)
+        {
+            Location = location;
+            PersonCount = personCount;
+            PhoneNumberCount = phoneNumberCount;
+        }
+    }
+}
